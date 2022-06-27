a92105fddc78cfa025b88fde60a6aedaf1be0f97
Caio <caiovictor@disroot.org>
2022-06-27 20:24:29 -0300

    tst

--- myapi.py
+++ myapi.py
@@ -39,7 +39,7 @@ def get_student_(*, student_id: int, name: Optional[str] = None, test : int):
             return students[student_id]
     return {"Data": "Not Found"}
 
-#### Request body and Post method
+#### Request body and Post method ####
 @app.post("/create-student/{student_id}")
 def create_student(student_id: int, student: Student):
     if student_id in students:
